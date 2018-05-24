b131e304ac9d4059f04d1f1591fb25676156759c
Indrek Ots <ots.indrek@gmail.com>
2018-05-24 16:59:06 +0300

    initial commit

---
+++ src/main/java/ee/concise/chat/gifchat/GifChatApplication.java
@@ -0,0 +1,12 @@
+package ee.concise.chat.gifchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GifChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GifChatApplication.class, args);
+	}
+}
---
+++ src/test/java/ee/concise/chat/gifchat/GifChatApplicationTests.java
@@ -0,0 +1,16 @@
+package ee.concise.chat.gifchat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GifChatApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
