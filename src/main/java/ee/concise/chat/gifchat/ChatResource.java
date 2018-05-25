2b4f3c5d4a58056814fa370ae4730fd13dcab93d
Indrek Ots <ots.indrek@gmail.com>
2018-05-25 16:51:05 +0300

    Create support for adding chat lines

--- src/main/java/ee/concise/chat/gifchat/ChatResource.java
+++ src/main/java/ee/concise/chat/gifchat/ChatResource.java
@@ -1,6 +1,7 @@
 package ee.concise.chat.gifchat;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +20,10 @@ public class ChatResource {
     @GetMapping
     public List<ChatLine> findAll() {
         return chatRepository.findAll();
+    }
+
+    @PostMapping
+    public ChatLine create(ChatLine chatLine) {
+        return chatRepository.save(chatLine);
     }
 }
--- src/test/java/ee/concise/chat/gifchat/ChatResourceTest.java
+++ src/test/java/ee/concise/chat/gifchat/ChatResourceTest.java
@@ -12,8 +12,12 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -37,7 +41,7 @@ class ChatResourceTest {
     @DisplayName("displays all chat lines")
     void findAll() throws Exception {
         when(chatRepository.findAll()).thenReturn(chatLines());
-        mvc.perform(get("/chat"))
+        mvc.perform(get("/api/chat"))
             .andDo(print())
             .andExpect(status().isOk())
             .andExpect(jsonPath("$", hasSize(2)));
@@ -48,5 +52,24 @@ class ChatResourceTest {
         ChatLine line2 = new ChatLine(2L, "nice", "Anon");
 
         return Arrays.asList(line1, line2);
+    }
+
+    @Test
+    @DisplayName("adds new chat line")
+    void create() throws Exception {
+        String payload = "{\n" +
+            "    \"author\": \"Indrek\",\n" +
+            "    \"keyword\": \"phew\"\n" +
+            "}";
+        when(chatRepository.save(any(ChatLine.class))).thenReturn(new ChatLine(1L, "phew", "Indrek"));
+
+        mvc.perform(post("/api/chat")
+            .content(payload))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.author", is("Indrek")))
+            .andExpect(jsonPath("$.id", is(1)))
+            .andExpect(jsonPath("$.keyword", is("phew")));
+
+        verify(chatRepository).save(any(ChatLine.class));
     }
 }
