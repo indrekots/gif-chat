889b7c27d086362d04b3031d969e1215d640cf69
Indrek Ots <ots.indrek@gmail.com>
2018-05-25 16:38:33 +0300

    Add '/api' to base path

--- src/main/java/ee/concise/chat/gifchat/ChatResource.java
+++ src/main/java/ee/concise/chat/gifchat/ChatResource.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/chat")
+@RequestMapping("/api/chat")
 public class ChatResource {
 
     private final ChatRepository chatRepository;
