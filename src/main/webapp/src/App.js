6223dad44ac202eb9029a94e6fc4c6cc15a8b369
Indrek Ots <ots.indrek@gmail.com>
2018-05-25 16:58:00 +0300

    Create heading for the app

--- src/main/webapp/src/App.js
+++ src/main/webapp/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -7,12 +7,12 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <img src={logo} className="App-logo" alt="logo"/>
+          <h1 className="App-title">Welcome to gif chat</h1>
+          <p className="App-intro">
+            This is a chat app where you can only use gifs
+          </p>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
