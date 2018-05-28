cde65e87707efce53e78859eeee776ac928a0d00
Indrek Ots <ots.indrek@gmail.com>
2018-05-28 14:16:01 +0300

    Remove app header

--- src/main/webapp/src/App.js
+++ src/main/webapp/src/App.js
@@ -67,10 +67,6 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo"/>
           <h1 className="App-title">Welcome to gif chat</h1>
         </header>
-        <p className="App-intro">
-          This is a chat app where you can only use gifs
-        </p>
-
         <div className="container">
           {!this.state.author &&
           <AuthorInput onSubmit={this.onAuthorSubmit}/>
